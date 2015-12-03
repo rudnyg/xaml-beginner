0d9f8b0458564f52aa628ba1d59467aac562283e
Rudny <Rudnyg@gmail.com>
2015-12-03 10:16:05 -0500

    Added POC for restaurant management app.
    Files added: MenuItem, Order, Table, RestuarntContext

---
+++ RestaurantManager.Models/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManager.Models
+{
+    public class Order
+    {
+        [Key]
+        public int  Id { get; set; }
+        public string SpecialRequest { get; set; }
+        public List<MenuItem> Items { get; set; }
+        public Table Table { get; set; }
+        public bool Complete { get; set; }
+        public bool Expedite { get; set; }
+        public override string ToString()
+        {
+            return $"{Items.Select(i => i.Title)}, ";
+        }
+    }
+}
