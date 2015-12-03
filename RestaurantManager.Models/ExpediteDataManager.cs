fcd31489c7520d4946ea42a3379ce66b16522647
Rudny <Rudnyg@gmail.com>
2015-12-03 10:49:01 -0500

    Added DataManger implementations For Expedite and Order
    ExpediteDataManger and OrderDataManager will be used to display data to
    the corresponding UI views.

---
+++ RestaurantManager.Models/ExpediteDataManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManager.Models
+{
+    public class ExpediteDataManager : DataManager
+    {
+        protected override void OnDataLoaded()
+        {
+            
+        }
+
+        public List<Order> OrderItems => base.Repository.Orders;
+    }
+}
---
+++ RestaurantManager.Models/OrderDataManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManager.Models
+{
+    public class OrderDataManager : DataManager
+    {
+        public List<MenuItem> MenuItems { get; set; }
+        public List<MenuItem> CurrentlySelectedMenuItems { get; set; }
+
+
+        protected override void OnDataLoaded()
+        {
+            MenuItems = Repository.StandardMenuItems;
+
+            CurrentlySelectedMenuItems = new List<MenuItem>
+            {
+                //simulate selecting multiple items 
+                MenuItems[3],
+                MenuItems[5]
+            };
+        }
+    }
+}
