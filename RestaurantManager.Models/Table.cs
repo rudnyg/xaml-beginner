0382ec68cdc0f12d96e2089ee113fd6b903b9a8f
Rudny <Rudnyg@gmail.com>
2015-12-14 15:43:53 -0500

    Add UI Styles for Main and Expedite page
    Main home buttons are round, Added template for data grid view

--- RestaurantManager.Models/Table.cs
+++ RestaurantManager.Models/Table.cs
@@ -11,5 +11,9 @@ namespace RestaurantManager.Models
         public int Id { get; set; }
         public string Description { get; set; }
 
+        public override string ToString()
+        {
+            return Description;
+        }
     }
 }
